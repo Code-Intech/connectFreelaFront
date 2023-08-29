2d83420410dc54779854f0d4643e36b2a5038440
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-08-29 19:18:26 -0300

    add menusidebar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MenuComponent from '@/components/MenuComponent.vue';
 import PaginationComponent from '@/components/PaginationComponent.vue';
+import TesteMenuSidebar from '@/components/TesteMenuSidebar.vue';
 
 
 const routes = [
