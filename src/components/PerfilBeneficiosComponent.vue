3b43427ec74d8c001d9b20c0068760777ef801de
Larissa-Santos16 <larisan970@gmail.com>
2023-09-14 22:05:15 -0300

    modal-pagamento

--- src/components/PerfilBeneficiosComponent.vue
+++ src/components/PerfilBeneficiosComponent.vue
@@ -1,35 +1,73 @@
 <template>
     <div class="col-10 d-inline-block" style="min-height: 80vh; max-height: 80vh">
-        <div class="d-flex">
+        <div class="col-10 d-inline-block" style="min-height: 80vh; max-height: 80vh">
+            <div class="d-flex">
 
-            <div class="flex-column ">
-                <div class="Title">
-                    <h1>Benefícios</h1>
+                <div class="flex-column ">
+                    <div class="Title">
+                        <h1>Benefícios</h1>
+                    </div>
+
+                    <div class="beneficiosList">
+                        <ul>
+                            <li>Beneficio 1</li>
+                            <li>Beneficio 2</li>
+                            <li>Beneficio 3</li>
+                            <li>Beneficio 4</li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="beneficiosList">
-                    <ul>
-                        <li>Beneficio 1</li>
-                        <li>Beneficio 2</li>
-                        <li>Beneficio 3</li>
-                        <li>Beneficio 4</li>
-                    </ul>
+                <div class="" style="padding: 450px">
+                    <div class="d-md-flex justify-content-md-end mt-5">
+                        <button class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal"
+                            data-bs-whatever="@getbootstrap">Assinar</button>
+                    </div>
                 </div>
             </div>
-
-            <div class="" style="padding: 450px">
-                <div class="d-md-flex justify-content-md-end mt-5">
-                <button class="btn btn-primary " type="button">Assinar</button>
-            </div>
-            </div>
-        </div>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-12 d-inline-block">
-                1 of 3
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-12 d-inline-block">
+                        1 of 3
+                    </div>
+                    <div class="col-2 d-inline-block">
+                        3 of 3
+                    </div>
                 </div>
-                <div class="col-2 d-inline-block">
-                3 of 3
+            </div>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Entre com as informações de Pagamento</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form>
+                                <div class="mb-3">
+                                    <label for="recipient-name" class="col-form-label">Número do Cartão:</label>
+                                    <input type="number" class="form-control" id="recipient-name">
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <label for="recipient-name" class="col-form-label">Data de Validade:</label>
+                                        <input type="date" class="form-control">
+                                    </div>
+                                    <div class="col">
+                                        <label for="recipient-name" class="col-form-label">CVV:</label>
+                                        <input type="number" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="recipient-name" class="col-form-label">Nome do titular:</label>
+                                    <input type="text" class="form-control" id="recipient-name">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Assinar</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
