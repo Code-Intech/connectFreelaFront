9334b6da5eb007187ad0f7e229d112648b3d5e2a
WilliamFraga21 <william.fraga@fatec.sp.gov.br>
2023-09-14 22:19:11 -0300

    Portfolio Component

--- src/components/PortfolioPerfilComponent.vue
+++ src/components/PortfolioPerfilComponent.vue
@@ -1,6 +1,110 @@
 <template>
-    <div class="col-10 h-100 d-inline-block">
+    <div class="col-10 ">
 
+        <div class="d-flex justify-content-center mt-5">
+            <div class=" ">
+                <div class="card border-5  rounded-3" style="max-width: 60rem; border-color: var(--purple-primary);">
+                    <div class="border-bottom border-black border-2  " style="min-height: 15rem; ">
+
+                        <div class="m-1 rounded-3 d-flex justify-content-center"
+                            style="background-color: var(--purple-primary);">
+                            <h5 class="h5 text-white">Seu Pollrtfólio</h5>
+                        </div>
+                        <!-- Card -->
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+                        <div class="card d-inline-block m-2 border border-black border-1" style="width: 18rem;">
+                            <div class="border-bottom border-black border-1">
+                                <img src="../assets/img/mamaco.png" class="card-img-top" alt="">
+
+                            </div>
+                            <div class="card-body d-grid gap-2">
+
+                                <button type="button" class="btn btn-danger"><font-awesome-icon icon="trash" />
+                                    Deletar</button>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            <form action="">
+                                <div class="input-group">
+                                    <input type="file" class="form-control btn btn-outline-primary" id="inputGroupFile04"
+                                        aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                                    <button class="btn btn-outline-secondary btn-success text-white" type="submit"
+                                        id="inputGroupFileAddon04">Enviar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
