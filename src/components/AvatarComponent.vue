39d684313bd7d94beeefbb27a377dfd08c028d0e
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-09-19 20:24:03 -0300

    (feat): add avatar

---
+++ src/components/AvatarComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <img :src="source" :alt="alt">
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AvatarComponent",
+  props: {
+    source: String,
+    alt: String
+  }
+})
+</script>
+
+<style scoped>
+  img {
+    width: 4rem;
+    height: 4rem;
+    border-radius: 50%;
+  }
+</style>
