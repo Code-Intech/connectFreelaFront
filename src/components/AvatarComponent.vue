e305b596d9c9007c73e7035eaee0a71fe747d849
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2023-09-19 21:30:42 -0300

    (feat): new props

--- src/components/AvatarComponent.vue
+++ src/components/AvatarComponent.vue
@@ -1,23 +1,52 @@
 <template>
-  <img :src="source" :alt="alt">
+  <img
+      :src="source ? source : userDefault"
+      :alt="alt ? alt : 'user image'"
+      :class="[source ? '' : 'userProfileBg']"
+      :style="{'height': height, 'width': width}"
+  >
 </template>
 
 <script>
-import {defineComponent} from 'vue'
 
-export default defineComponent({
+import personFill from '@/assets/svg/person-fill.svg'
+
+export default {
   name: "AvatarComponent",
+  data() {
+    return {
+      userDefault: personFill
+    }
+  },
   props: {
-    source: String,
+    source: {
+      type: String,
+      required: false
+    },
+    width: {
+      type: String,
+      default: "4rem"
+    },
+    height: {
+      type: String,
+      default: "4rem"
+    },
     alt: String
   }
-})
+}
 </script>
 
 <style scoped>
   img {
-    width: 4rem;
-    height: 4rem;
     border-radius: 50%;
   }
+
+  .userProfileBg {
+    background: #d3d3d3;
+  }
+
+  .userProfileBg path {
+    background-color: red;
+  }
+
 </style>
