e78804a32d5d2a47cc29c9b28466c955ebdc2f4f
WilliamFraga21 <william.fraga@fatec.sp.gov.br>
2023-09-22 21:39:59 -0300

    View Perfil

--- src/components/PerfilComponent.vue
+++ src/components/PerfilComponent.vue
@@ -1,7 +1,17 @@
 <template >
 
-<div>
-    
+<div class="border-end border-black border-2 border-top " style="height: 88vh; background-color: var(--purple-tertiary);">
+    <div class="d-flex justify-content-center ">
+        <img class="mt-4 rounded" src="../assets/img/mamaco.jpeg" alt="" style="width: 15rem; height: 15rem;">
+
+    </div>
+
+    <div class="ms-5 me-5 mt-3 d-grid gap-2">
+        <button class="d-block btn text-white" style="border: 2px solid; border-color: var(--purple-quaternary);">Sobre</button>
+        <button class="d-block btn text-white" style="border: 2px solid; border-color: var(--purple-quaternary);">DashBoard</button>
+        <button class="d-block btn text-white" style="border: 2px solid; border-color: var(--purple-quaternary);">Portifólio</button>
+    </div>
+
 </div>
 
 
--- src/components/UserPortifolioComonent.vue
+++ src/components/UserPortifolioComonent.vue
@@ -1,37 +1,107 @@
 <template >
-
     <div>
-        
+
+
+
+
+        <div class="card" style="width: 20rem; ">
+            <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Album de Macaco</h5>
+                <div id="carouselExample" class="carousel slide">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <div>
+                    <button class="btn btn-primary mt-3" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+                        Ver Album
+                    </button>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- 
+        <p>
+            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample"
+                aria-expanded="false" aria-controls="collapseWidthExample">
+                Toggle width collapse
+            </button>
+        </p> -->
+        <div style="min-height: 120px;">
+            <div class="collapse collapse-horizontal" id="collapseWidthExample">
+                <div class="card card-body" style="max-width: 70rem;">
+                    <div id="carouselExample" class="carousel slide">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../assets/img/mamaco.png" class="d-block w-100" alt="">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
     </div>
-    
-    
-    
-    </template>
+</template>
         
-        <script>
+<script>
+
+
+export default {
+    name: "UserPortifolioComonent",
+    methods: {
+
+    },
+    components: {
+
+
+
+    },
+
+}
+
+
+</script>
         
-        
-        export default {
-            name: "UserPortifolioComonent",
-            methods: {
-        
-            },
-            components: {
-        
-        
-        
-            },
-        
-        }
-        
-        
-        </script>
-        
-        <style scoped >
-        
-        @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lobster&display=swap');
-        
-        
-    
-        
-        </style>
+<style scoped >@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&family=Lobster&display=swap');</style>
