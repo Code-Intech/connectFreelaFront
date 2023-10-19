8ced23e3992d33159c453a8fc6bac83b13f0c5ba
William Victor Soares Silva Marques Fraga <william.fraga@fatec.sp.gov.br>
2023-10-19 20:54:14 -0300

    Ajuste no botão menu USER

--- src/components/NavBars/LandingNavBarComponent.vue
+++ src/components/NavBars/LandingNavBarComponent.vue
@@ -11,49 +11,73 @@
           FAQ
         </button>
 
-        <div class="dropstart  " @click="getinfo()">
-          <a class="   " style="" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <!-- <font-awesome-icon icon="user" /> -->
-            <!-- <img class=" rounded-circle" style="max-width: 30px;max-height: 40px;" src="@/assets/img/avatar.jpg" alt=""> -->
-            <avatar :source="store.getters.StateAvatar" width="2rem" height="2rem" />
-          </a>
 
-          <ul class="dropdown-menu border border-0 ">
+        <div>
 
-            <div class="card" style="width: 18r">
-              <avatar :source="store.getters.StateAvatar" height="300px" width="284px" class="card-img-top"
-                :rounded="false" />
-              <div class="card-body">
-                <h5 class="card-title">William Fraga:</h5>
+          <div v-if="store.getters.GetToken == null">
 
-                <div class="d-flex flex-column mb-1 gap-1">
-                  <button @click="$router.push({ path: '/perfilbeneficios' })" class="btn btn-primary"
-                    style="background-color: goldenrod;border: none;"><font-awesome-icon :icon="['fas', 'medal']" />
-                    Plus</button>
-                  <button @click="$router.push({ path: '/userperfil' })" class="btn btn-primary "><font-awesome-icon
-                      :icon="['fas', 'user-large']" /> Perfil</button>
-                  <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'envelope']" /> Notificações</button>
+            <div class="  " @click="$router.push({ path: '/login' })">
+              <a class="   " style="" type="button">
+                <!-- <font-awesome-icon icon="user" /> -->
+                <!-- <img class=" rounded-circle" style="max-width: 30px;max-height: 40px;" src="@/assets/img/avatar.jpg" alt=""> -->
+                <avatar :source="store.getters.StateAvatar" width="2rem" height="2rem" />
+              </a>
+
+            </div>
+          </div>
 
 
+          <div v-else class="dropstart  " @click="getinfo()">
+            <a class="   " style="" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <!-- <font-awesome-icon icon="user" /> -->
+              <!-- <img class=" rounded-circle" style="max-width: 30px;max-height: 40px;" src="@/assets/img/avatar.jpg" alt=""> -->
+              <avatar :source="store.getters.StateAvatar" width="2rem" height="2rem" />
+            </a>
 
-                  <button @click="$router.push({ path: '/perfil' })" class="btn btn-primary"><font-awesome-icon
-                      :icon="['fas', 'gear']" /> Config</button>
+            <ul class="dropdown-menu border border-0 ">
+
+              <div class="card" style="width: 18r">
+                <avatar :source="store.getters.StateAvatar" height="300px" width="284px" class="card-img-top"
+                  :rounded="false" />
+                <div class="card-body">
+                  <h5 class="card-title">William Fraga:</h5>
+
+                  <div class="d-flex flex-column mb-1 gap-1">
+                    <button @click="$router.push({ path: '/perfilbeneficios' })" class="btn btn-primary"
+                      style="background-color: goldenrod;border: none;"><font-awesome-icon :icon="['fas', 'medal']" />
+                      Plus</button>
+                    <button @click="$router.push({ path: '/userperfil' })" class="btn btn-primary "><font-awesome-icon
+                        :icon="['fas', 'user-large']" /> Perfil</button>
+                    <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'envelope']" />
+                      Notificações</button>
+
+
+
+                    <button @click="$router.push({ path: '/perfil' })" class="btn btn-primary"><font-awesome-icon
+                        :icon="['fas', 'gear']" /> Config</button>
 
 
 
 
-                  <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'circle-half-stroke']" />
-                    Tema</button>
-                  <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'comments-dollar']" />
-                    Memsagens</button>
-                  <button @click="LogOutM()" class="btn btn-primary logoff" style="border: none;"><font-awesome-icon
-                      :icon="['fas', 'power-off']" /></button>
+                    <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'circle-half-stroke']" />
+                      Tema</button>
+                    <button class="btn btn-primary"><font-awesome-icon :icon="['fas', 'comments-dollar']" />
+                      Memsagens</button>
+                    <button @click="LogOutM()" class="btn btn-primary logoff" style="border: none;"><font-awesome-icon
+                        :icon="['fas', 'power-off']" /></button>
+                  </div>
                 </div>
               </div>
-            </div>
 
-          </ul>
+            </ul>
+          </div>
+
         </div>
+
+
+
+
+
       </div>
     </div>
 
@@ -101,6 +125,9 @@ export default {
     },
 
 
+  },
+  login() {
+    this.$router.push("/login");
   },
 }
 </script>
