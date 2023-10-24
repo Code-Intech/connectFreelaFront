86232bbce1722280945fc2793b6d475b49034e2a
Larissa-Santos16 <larisan970@gmail.com>
2023-10-23 22:55:39 -0300

    Cards Responsivos - CardServico

--- src/components/CardsServicosComponent.vue
+++ src/components/CardsServicosComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-4" v-for="(valores, index) in valor" :key="valores[index]">
+            <div class="col-lg-4" v-for="(valores, index) in valor" :key="valores[index]">
                 <div class="mt-3 ms-2 d-flex gap-4 flex-wrap">
                     <div class="card" style="width: 25rem;">
 
@@ -9,99 +9,116 @@
                             <p>{{ valores.servicoInfo.Titulo_Servico }}</p>
                         </div>
                         <div class="card-body">
-                            <div clss="d-flex">
-                                <AvatarComponent/>
-                                <text class="p-2 fst-italic">{{ valores.contratante.Nome_Completo }}</text>
+                            <div clss="container">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <AvatarComponent />
+                                    </div>
+                                    <div class="col">
+                                        <text class="fst-italic">{{ valores.contratante.Nome_Completo }}</text>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="d-flex texts">
-                                <div>
-                                    <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
-                                        Profissão:
-                                    </h6>
-                                    <h6 class="fst-italic" style="font-size: smaller;">
-                                        <div v-for="(val, index2) in valores.servicoProfessions" :key="val[index2]">
-                                            {{ val.Profissao }};<br>
+                            <div class="container texts">
+                                <div class="row">
+                                    <div class="col-lg-8 justify-content-start">
+                                        <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
+                                            Profissão:
+                                        </h6>
+                                        <h6 class="fst-italic" style="font-size: smaller;">
+                                            <div v-for="(val, index2) in valores.servicoProfessions" :key="val[index2]">
+                                                {{ val.Profissao }};<br>
+                                            </div>
+                                        </h6>
+                                    </div>
+                                    <div class="col justify-content-start ms-5">
+                                        <h6 class="fst-italic" style="font-size: smaller;">
+                                            <font-awesome-icon class="me-1" icon="money-bill" />{{
+                                                valores.servicoInfo.Estimativa_Valor }}
+                                        </h6>
+                                        <h6 class="fst-italic" style="font-size: smaller;">
+                                            {{ valores.servicoInfo.Remoto_Presencial }}
+                                        </h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="border-bottom border-black justify-content-start">
+                                <div class="container">
+                                    <div class="row">
+                                        <div>
+                                            <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">Data
+                                                de
+                                                Publicação:
+                                            </h6>
+                                            <h6 class="fst-italic" style="font-size: smaller;">
+                                                12/10/2003
+                                            </h6>
                                         </div>
-                                    </h6>
-                                </div>
-                                <div class="ms-5">
+                                        <div class="col-7">
+                                            <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
+                                                Data de Início:
+                                            </h6>
+                                            <h6 class="fst-italic" style="font-size: smaller;">
+                                                {{ valores.servicoInfo.Data_Inicio }}
+                                            </h6>
+                                        </div>
 
-                                    <h6 class="fst-italic" style="font-size: smaller;">
-                                        <font-awesome-icon class="me-1" icon="money-bill" />{{
-                                            valores.servicoInfo.Estimativa_Valor }}
-                                    </h6>
-                                    <h6 class="fst-italic" style="font-size: smaller;">
-                                        {{ valores.servicoInfo.Remoto_Presencial }}
-                                    </h6>
-                                </div>
-                            </div>
-                            <div class="m-2 border-bottom border-black justify-content-between">
-                                <div>
-                                    <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">Data de
-                                        Publicação:
-                                    </h6>
-                                    <h6 class="fst-italic" style="font-size: smaller;">
-                                        12/10/2003
-                                    </h6>
-                                </div>
-                                <div class="d-flex">
-                                    <div>
-                                        <h6 class="d-flex fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
-                                            Data de Início:
-                                        </h6>
-                                        <h6 class="fst-italic" style="font-size: smaller;">
-                                            {{ valores.servicoInfo.Data_Inicio }}
-                                        </h6>
-                                    </div>
-
-                                    <div>
-                                        <h6 class="d-flex fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
-                                            Estimativa de Término:
-                                        </h6>
-                                        <h6 class="fst-italic" style="font-size: smaller;">
-                                            {{ valores.servicoInfo.Estimativa_de_termino }}
-                                        </h6>
+                                        <div class="col">
+                                            <h6 class="fst-italic" style="font-size: smaller; color:rgb(103, 102, 102)">
+                                                Estimativa de Término:
+                                            </h6>
+                                            <h6 class="fst-italic" style="font-size: smaller;">
+                                                {{ valores.servicoInfo.Estimativa_de_termino }}
+                                            </h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="d-flex justify-content-between">
-                                <p class="text-success">
-                                    Localidade:
-                                </p>
-                                <p class="justify-content-end">
-                                    {{ valores.localidade.Cidade }}, {{ valores.localidade.Estado }}, {{valores.localidade.Bairro }}
-                                </p>
+
+                            <div class="container">
+                                <div class="row">
+                                    <div class="row">
+                                        <p class="col-6 text-success">
+                                            Localidade:
+                                        </p>
+                                        <p class="col text-right">
+                                            {{ valores.localidade.Cidade }}, {{ valores.localidade.Estado }},
+                                            {{ valores.localidade.Bairro }}
+                                        </p>
+                                    </div>
+
+                                    <div class="row">
+                                        <p class="col-8 text-success">
+                                            Distancia estipulada:
+                                        </p>
+                                        <p class="col">
+                                            {{ valores.servicoInfo.Estimativa_de_distancia }}Km
+                                        </p>
+                                    </div>
+
+                                    <div class="row">
+                                        <p class="col-9 text-success">
+                                            Estimativa de Idade:
+                                        </p>
+                                        <p class="col">
+                                            {{ valores.servicoInfo.Estimativa_Idade }}
+                                        </p>
+                                    </div>
+
+                                    <div class="row">
+                                        <p class="col-9 text-success">
+                                            Propostas enviadas:
+                                        </p>
+                                        <p class="col">
+                                            35
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="d-flex justify-content-between">
-                                <p class="text-success">
-                                    Distancia estipulada:
-                                </p>
-                                <p>
-                                    {{ valores.servicoInfo.Estimativa_de_distancia }}
-                                </p>
-                            </div>
-
-                            <div class="d-flex justify-content-between">
-                                <p class="text-success">
-                                    Estimativa de Idade:
-                                </p>
-                                <p>
-                                    {{ valores.servicoInfo.Estimativa_Idade }}
-                                </p>
-                            </div>
-
-                            <div class="d-flex justify-content-between">
-                                <p class="text-success">
-                                    Propostas enviadas:
-                                </p>
-                                <p class="text">
-                                    35
-                                </p>
-                            </div>
-
-                            <div class="d-flex justify-content-between">
+                            <div class="justify-content-between">
                             </div>
 
                             <p class="card-text border-top border-bottom border-black">{{ valores.servicoInfo.Desc }}
@@ -124,18 +141,20 @@
 
                             </div>
 
-                            <div class="d-flex justify-content-between">
-                                <div class="buttuns mt-3">
+                            <div class="container">
+                                <div class="row justify-content-between">
+                                <div class="col-6 buttuns mt-3">
                                     <button type="button" class="btn btn-primary" data-bs-toggle="modal"
                                         data-bs-target="#exampleModal"
                                         style="background-color: rgb(11, 217, 11); font-size: small; border:none;">Enviar
                                         Proposta</button>
                                 </div>
-                                <div class="buttuns mt-3">
+                                <div class="col buttuns mt-3">
                                     <button @click="$router.push({ path: '/ViewPropostas' })" type="button"
                                         class="btn btn-primary"
                                         style="background-color: rgb(247, 162, 5); font-size: small; border:none;">Ver
                                         Propostas</button>
+                                    </div>
                                 </div>
                             </div>
 
@@ -196,12 +215,12 @@ export default {
 
     methods: {
         validateOnBack: Boolean,
-        ...mapActions(["getServico", "getInfoServico", "CreateServico"]),
+        ...mapActions(["getServico", "getInfoServico", "CreateServico", "formatDate"]),
         ...mapGetters([]),
 
         getAllServicos() {
             this.valor = store.getters.StateServico
-        },
+        }
     },
 
 }
